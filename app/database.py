4266c1d85ca18ce99ca50cfe13337b84be7ceef9
yukaoi <aoyk.ooiooi@gmail.com>
2026-04-08 00:31:31 +0900

    DATABASE_URLのupdate

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./app.db"
+DATABASE_URL = "sqlite:////data/app.db"
 
 engine = create_engine(
     DATABASE_URL,
